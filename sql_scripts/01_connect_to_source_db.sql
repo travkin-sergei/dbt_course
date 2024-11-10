a576e84d82fcc602cfd858734ccdff3521a0bfd8
amelinvladimir <amelinvd@gmail.com>
2024-11-10 14:28:17 +0300

    формирование в бд staging слоя

---
+++ sql_scripts/01_connect_to_source_db.sql
@@ -0,0 +1,25 @@
+-- сначала создаем БД dbt_course и открываем sql редактор в этой БД и выполняем дальнейший скрипт
+
+create schema staging;
+create schema intermediate;
+create schema marts;
+
+select * from pg_catalog.pg_available_extensions;
+
+create extension postgres_fdw;
+
+drop server demo_pg cascade;
+create server demo_pg foreign data wrapper postgres_fdw options (
+	host 'localhost',
+	dbname 'demo',
+	port '5432'
+);
+
+create user mapping for postgres server demo_pg options (
+	user 'postgres',
+	password 'mysecretpassword'
+);
+
+drop schema if exists demo_src;
+create schema demo_src authorization postgres;
+import foreign schema bookings from server demo_pg into demo_src;
